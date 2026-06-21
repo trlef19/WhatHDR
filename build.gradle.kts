e39e04d8795c176ecf446a1afa076470ab385b29
trlef19 <lefteris592@gmail.com>
2026-06-21 13:18:04 +0300

    updated gradle dependencies, properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,10 +6,10 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:9.2.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.10")
-        classpath("com.mikepenz.aboutlibraries.plugin:aboutlibraries-plugin:11.2.3")
+        classpath("com.mikepenz.aboutlibraries.plugin:aboutlibraries-plugin:14.2.1")
     }
 }
 plugins {
     id("com.google.devtools.ksp") version "2.3.9" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.0" apply false
 }
