1e5a21ad6ec6faffcee26b0f7246602aef3411d2
trlef19 <lefteris592@gmail.com>
2026-07-30 12:12:40 +0300

    updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,12 +4,12 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:9.3.0")
+        classpath("com.android.tools.build:gradle:9.3.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.10")
         classpath("com.mikepenz.aboutlibraries.plugin:aboutlibraries-plugin:14.2.1")
     }
 }
 plugins {
     id("com.google.devtools.ksp") version "2.3.9" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.4.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
 }
--- whathdr/build.gradle.kts
+++ whathdr/build.gradle.kts
@@ -46,14 +46,13 @@ android {
 }
 
 dependencies {
-    implementation("androidx.compose.material3:material3:1.5.0-alpha24")
+    implementation("androidx.compose.material3:material3:1.5.0-alpha25")
     implementation("androidx.compose.ui:ui-tooling-preview:1.11.4")
     implementation("androidx.compose.ui:ui:1.11.4")
     implementation("com.google.accompanist:accompanist-flowlayout:0.36.0")
     implementation("androidx.core:core-ktx:1.19.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.11.0")
     implementation("androidx.activity:activity-compose:1.13.0")
-    implementation(platform("androidx.compose:compose-bom:2026.06.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
