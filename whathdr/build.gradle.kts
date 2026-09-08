6b22e892074bbb225c541083ac217e80f4486c6b
trlef19 <lefteris592@gmail.com>
2026-09-08 15:57:16 +0300

    updated version code

--- whathdr/build.gradle.kts
+++ whathdr/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.app.whathdr"
         minSdk = 34
         targetSdk = 37
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
